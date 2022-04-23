df61b699f506a6bc3c74389db3011963766c479c
Zeio Nara <zeionara@gmail.com>
2022-04-24 01:16:03 +0300

    Updated static pages

---
+++ js/about.7c6cda95.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcountdown"]=self["webpackChunkcountdown"]||[]).push([[443],{4335:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(3396);const c={class:"about"},e=(0,a._)("h1",null,"This is an about page",-1),o=[e];function r(n,u){return(0,a.wg)(),(0,a.iD)("div",c,o)}var s=t(89);const i={},d=(0,s.Z)(i,[["render",r]]);var f=d}}]);
+//# sourceMappingURL=about.7c6cda95.js.map
